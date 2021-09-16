e4409b724f57ad229ea616e748f25b438c9b86fc
Sourabh Patil <patilsourabh45@gmail.com>
2021-09-16 23:51:04 +0530

    edited

--- database/schema/user.js
+++ database/schema/user.js
@@ -2,12 +2,12 @@ const Schema = require('mongoose').Schema;
 
 const userSchema = new Schema({
   name: String,
-  phonenumber:String,
   email: {
     type : String,
     required: true,
     unique : true
   },
+  phonenumber:String,
   password: String,
   noOfOrders: { type: Number, "default": 0 },
   password: { type: String, required: true, "default": "Helloworld123#"}
